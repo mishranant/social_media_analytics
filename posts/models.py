e10b258e574a387a3d3bd7a78f26c27ab741c548
Anant Mishra <anant.mishra.student@gmail.com>
2023-10-01 16:20:28 +0530

    added create and analysis APIs: basics

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    def __str__(self):
+        return str(self.id + ': ' + self.content)
+    
+    content=models.TextField()
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Post
+import json, re
 
+#  Post Creation (POST /api/v1/posts/): Accept a JSON payload with text content and a unique identifier.
+@csrf_exempt
 def create(request):
-    # request.POST['key']
-    return render(request, 'posts/create.html', {})
+    if request.method == 'POST':
+        requestBody = json.loads(request.body)
+        Post(id=requestBody['id'], content=requestBody['content']).save()
+        return HttpResponse('successfully created a new post')
 
+# Post Analysis (GET /api/v1/posts/{id}/analysis/): Provide an analysis endpoint that returns the number of words and average word length in a post.
+@csrf_exempt
 def analysis(request, post_id):
-    return render(request, 'posts/analysis.html', {})
+    post = Post.objects.get(pk=post_id)
+    words = re.findall(r'\w+', post.content)
+    word_count = len(words)
+    avg_word_length = sum([len(x) for x in words]) / word_count
+    return render(request, 'posts/analysis.html', {'content': post.content, 'word_count': word_count, 'avg_word_length': avg_word_length})
